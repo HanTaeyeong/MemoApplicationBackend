a16402be775dae0b5ec0945cb67ca95ab2dedb43
HanTaeyeong <gksxodud2721@naver.com>
2021-03-07 13:23:11 +0900

    backend

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
 import mongoose from 'mongoose';
 
-// import serve from 'koa-static'
-// import path from 'path';
-// import send from 'koa-send';
+import serve from 'koa-static'
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api'
 
@@ -35,15 +35,15 @@ router.use('/api', api.routes())
 
 app.use(router.routes()).use(router.allowedMethods())
 
-// const buildDirectory = path.resolve(__dirname, '../frontend/build');
-// app.use(serve(buildDirectory));
-// app.use(async ctx => {
-//     if (ctx.status = 404 && ctx.path.indexOf('/api') !== 0) {
-//         await send(ctx, 'index.html', { root: buildDirectory })
-//     }
-// })
+const buildDirectory = path.resolve(__dirname, '../frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+    if (ctx.status = 404 && ctx.path.indexOf('/api') !== 0) {
+        await send(ctx, 'index.html', { root: buildDirectory })
+    }
+})
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri).then(() => {
