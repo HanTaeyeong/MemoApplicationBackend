0951936b031b86a1ebce96b40eefa5c4a195d67f
HanTaeyeong <gksxodud2721@naver.com>
2021-06-12 15:00:02 +0900

    port 443

--- main.js
+++ main.js
@@ -5,11 +5,8 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
-
 import api from './api'
-
 import jwtMiddleware from './lib/jwtMiddleware';
-import { nextTick } from 'process';
 
 const app = new Koa();
 const router = new Router();
@@ -23,7 +20,7 @@ router.use('/api', api.routes())
 app.use(router.routes()).use(router.allowedMethods())
 
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
