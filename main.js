5c8a525d4949e60cbaf1e743531f66a223808308
HanTaeyeong <gksxodud2721@naver.com>
2021-06-12 14:30:06 +0900

    port 80

--- main.js
+++ main.js
@@ -3,12 +3,8 @@ require('dotenv').config();
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
-// import cors from '@koa/cors'; removed when deployment state 
 import mongoose from 'mongoose';
 
-import serve from 'koa-static'
-import path from 'path';
-import send from 'koa-send';
 
 import api from './api'
 
@@ -18,7 +14,6 @@ import { nextTick } from 'process';
 const app = new Koa();
 const router = new Router();
 
-// app.use(cors());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 
@@ -28,19 +23,7 @@ router.use('/api', api.routes())
 app.use(router.routes()).use(router.allowedMethods())
 
 
-const port = process.env.PORT || 5050;
-
-// const buildDirectory = path.resolve(__dirname, './client/build');
-// app.use(serve(buildDirectory));
-
-// app.use(async ctx => {
-//     if ((+ctx.port) !== port) {
-//         ctx.redirect(`:${port}`);
-//     }
-//     if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
-//         await send(ctx, 'index.html', { root: buildDirectory })
-//     }
-// })
+const port = process.env.PORT || 80;
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
