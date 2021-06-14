7c4800729af79b5549dae23fb93c36f1615f1b73
HanTaeyeong <rex@quebon.com>
2021-06-14 14:27:28 +0900

    delete patch changed to post method

--- main.js
+++ main.js
@@ -29,16 +29,16 @@ app.use(router.routes()).use(router.allowedMethods())
 const port = process.env.PORT || 5347;
 
 const uri = process.env.MONGODB_URI;
-const key = fs.readFileSync(path.join(__dirname, 'cert', 'key.pem'))
-const cert = fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-const sslServer = https.createServer({
-    key,
-    cert
-}, app.listen)
+// const key = fs.readFileSync(path.join(__dirname, 'cert', 'key.pem'))
+// const cert = fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+// const sslServer = https.createServer({
+//     key,
+//     cert
+// }, app.listen)
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    sslServer.listen(port, () => {
-        console.log('sslServer is running at port ' + port)
+    app.listen(port, () => {
+        console.log('server is running at port ' + port)
     });
 }).catch(e => console.log(e));
 
